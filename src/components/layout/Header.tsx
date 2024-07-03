8cfa8e08223b84a64eebed2af85d6cf0ef3c2f9e
hasan814 <h.mousavi910@gmail.com>
2024-07-03 11:42:27 +0330

    Finished Header Section

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,26 @@
+import Socials from "@/modules/Socials";
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
+          <Link href={"/"}>
+            <Image
+              src={"/image.png"}
+              priority
+              width={100}
+              height={48}
+              alt="logo"
+            />
+          </Link>
+          <Socials />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/modules/ParticlesContainer.tsx
+++ src/components/modules/ParticlesContainer.tsx
@@ -1,73 +1,90 @@
+// ParticlesContainer.tsx
 "use client";
 
-import { useCallback } from "react";
-import { Particles } from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesContainer = () => {
-  // ============ Particles ===============
-  const particlesInit = useCallback(async (engine: any) => {
-    console.log("particlesInit", engine);
-    await loadFull(engine);
-    console.log("Particles loaded successfully");
+  const [init, setInit] = useState(false);
+
+  // Initialize tsParticles
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
   }, []);
 
-  const particlesLoaded = useCallback(async (container: any) => {
-    console.log("particlesLoaded", container);
-  }, []);
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
 
-  // ============ Rendering ===============
-  return (
-    <Particles
-      className="w-full h-full absolute translate-z-0"
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        fullScreen: { enable: false },
-        background: { color: { value: "" } },
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onClick: { enable: false, mode: "push" },
-            onHover: { enable: true, mode: "repulse" },
-            resize: true,
-          },
-          modes: {
-            push: { quantity: 90 },
-            repulse: { distance: 200, duration: 0.4 },
-          },
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fullScreen: { enable: false },
+      background: { color: { value: "" } },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: { enable: false, mode: "push" },
+          onHover: { enable: true, mode: "repulse" },
+          resize: true,
         },
-        particles: {
-          color: { value: "#e68e2e" },
-          links: {
-            color: "#f5d393",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: { enable: true },
-          move: {
-            direction: "none",
-            enable: true,
-            outModes: { default: "bounce" },
-            random: false,
-            speed: 1,
-            straight: false,
-          },
-          number: {
-            density: { enable: true, area: 800 },
-            value: 80,
-          },
-          opacity: { value: 0.5 },
-          shape: { type: "circle" },
-          size: { value: { min: 1, max: 5 } },
+        modes: {
+          push: { quantity: 90 },
+          repulse: { distance: 200, duration: 0.4 },
         },
-        detectRetina: true,
-      }}
-    />
+      },
+      particles: {
+        color: { value: "#e68e2e" },
+        links: {
+          color: "#f5d393",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        collisions: { enable: true },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: { default: OutMode.bounce },
+          random: false,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: { enable: true, area: 800 },
+          value: 80,
+        },
+        opacity: { value: 0.5 },
+        shape: { type: "circle" },
+        size: { value: { min: 1, max: 5 } },
+      },
+      detectRetina: true,
+    }),
+    []
   );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return null;
 };
 
 export default ParticlesContainer;
